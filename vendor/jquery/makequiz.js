63f13f26e3311c55f8746afac3938e535d5fc1be
HaidarAliN <haidaralinehme@gmail.com>
2021-09-25 19:11:40 +0300

    more features for make quiz page

--- vendor/jquery/makequiz.js
+++ vendor/jquery/makequiz.js
@@ -1,5 +1,5 @@
+let index = 0; //will be changed with question id from the databse
 $(document).ready(function () {
-    console.log('ok');
     var type_conditon = false;
     var question_valid = false;
     var mcq_valid = false;
@@ -9,17 +9,19 @@ $(document).ready(function () {
     var ans3_valid = false;
     var mcq_rightans_valid = false;
 
-    $('ul.product-info li a').click(function (event) {
+    $('ul.product-info li a').click(function (event) { //prevent link default action
         event.preventDefault();
         $(this).next('div').slideToggle(200);
     });
 
-    $("#tor").hide();
+    $("#tor").hide(); //initial state
     $("#mcq").hide();
     $("#addquestiondiv").hide();
     $("#questionstable").hide();
+    $("#editmcqdiv").hide();
+    $("#edittofdiv").hide();
 
-    $.fn.reset = function() {
+    $.fn.reset = function () { //reset for changing question type
         mcq_valid = false;
         tof_valid = false;
         ans1_valid = false;
@@ -29,7 +31,18 @@ $(document).ready(function () {
         $('#tofbtn').html('Choose Ans');
     };
 
-    $("#create_quiz_btn").click(function () {
+    $.fn.hardReset = function () { //reset after adding a question
+        $.fn.reset();
+        $('#typebtn').html('Choose type');
+        $('#questioncontent').val("");
+        $('#ans1').val("");
+        $('#ans2').val("");
+        $('#ans3').val("");
+        $("#tor").hide();
+        $("#mcq").hide();
+    };
+
+    $("#create_quiz_btn").click(function () { //create quiz btn
         var name = $('#quizname').val();
         if (name.length > 3) {
 
@@ -42,7 +55,7 @@ $(document).ready(function () {
         }
     });
 
-    $(".atype").click(function () {
+    $(".atype").click(function () { //choosing type action
         var type = $(this).html();
         $.fn.reset();
         mcq_rightans_valid = false;
@@ -57,17 +70,149 @@ $(document).ready(function () {
         }
     });
 
-    $(".mcqans").click(function () {
+    $(".mcqans").click(function () { //mcq dropdown action
         var value = $(this).html();
         $('#mcqansb').html(value);
     });
 
-    $(".tofans").click(function () {
+    $(".upmcqans").click(function () { //edit mcq dropdown action
+        var value = $(this).html();
+        $('#upmcqansb').html(value);
+    });
+
+    $(".tofans").click(function () { //tof dropdown action
         var value = $(this).html();
         $('#tofbtn').html(value);
     });
 
-    $("#addqbtn").click(function () {
+    $(".uptofans").click(function () { //edit tof dropdown action
+        var value = $(this).html();
+        $('#uptofansb').html(value);
+    });
+
+    $.fn.update_mcq_btn = function (tr_id) { //update mcq fuction
+        $("#upmcqbtn").click(function () {
+            var question = $('#mcqquestionupdate').val();
+            var ans1 = $('#firstmcqansup').val();
+            var ans2 = $('#secondmcqansup').val();
+            var ans3 = $('#thirdmcqansup').val();
+            var right_ans = $('#upmcqansb').html();
+
+            var question_valid = false;
+            var ans1_valid = false;
+            var ans2_valid = false;
+            var ans3_valid = false;
+
+            if (question.length > 4) {
+                question_valid = true;
+                $('#mcqquestionupdate').removeClass("alert-danger");
+            } else {
+                question_valid = false;
+                $('#mcqquestionupdate').addClass("alert-danger");
+            }
+
+            if (ans1.length > 0) {
+                ans1_valid = true;
+                $('#firstmcqansup').removeClass("alert-danger");
+            } else {
+                ans1_valid = false;
+                $('#firstmcqansup').addClass("alert-danger");
+            }
+
+            if (ans2.length > 0) {
+                ans2_valid = true;
+                $('#secondmcqansup').removeClass("alert-danger");
+            } else {
+                ans2_valid = false;
+                $('#secondmcqansup').addClass("alert-danger");
+            }
+
+            if (ans3.length > 0) {
+                ans3_valid = true;
+                $('#thirdmcqansup').removeClass("alert-danger");
+            } else {
+                ans3_valid = false;
+                $('#thirdmcqansup').addClass("alert-danger");
+            }
+
+            if (question_valid && ans1_valid && ans2_valid && ans3_valid) {
+                $(tr_id).find("td:eq(0)").html(question);
+                $(tr_id).find("td:eq(1)").html(ans1);
+                $(tr_id).find("td:eq(2)").html(ans2);
+                $(tr_id).find("td:eq(3)").html(ans3);
+                $(tr_id).find("td:eq(4)").html(right_ans);
+                $("#editmcqdiv").hide();
+                $('html, body').animate({
+                    scrollTop: $(tr_id).offset().top
+                }, 2000);
+            }
+        });
+    };
+
+    $.fn.update_tof_btn = function (tr_id) { //update tof fuction
+        $("#uptofbtn").click(function () {
+            var question = $('#tofquestionupdate').val();
+            var right_ans = $('#uptofansb').html();
+            if (question.length > 4) {
+                $(tr_id).find("td:eq(0)").html(question);
+                $(tr_id).find("td:eq(4)").html(right_ans);
+                $("#edittofdiv").hide();
+                $('html, body').animate({
+                    scrollTop: $(tr_id).offset().top
+                }, 2000);
+                $('#tofquestionupdate').removeClass("alert-danger");
+            } else {
+                $('#tofquestionupdate').addClass("alert-danger");
+            }
+        });
+    };
+
+    $.fn.removebtnlistener = function () { //table remove item listner initialiser
+        $(".removebtn").click(function () {
+            var id = $(this).attr('id');
+            const myArr = id.split("_");
+            var id_num = myArr[2];
+            var tr_id = "#tr_" + id_num;
+            $(tr_id).remove();
+        });
+    };
+
+    $.fn.editbtnlistener = function () { //table edit item listner initialiser
+        $(".editbtn").click(function () {
+            var id = $(this).attr('id');
+            const myArr = id.split("_");
+            var id_num = myArr[2];
+            var tr_id = "#tr_" + id_num;
+            var btn_type = $(this).attr('id');
+            btn_type = btn_type.split("_");
+            btn_type = btn_type[0];
+            if (btn_type == 'tof') {
+                console.log("tofffffff");
+                $('#tofquestionupdate').val($(tr_id).find("td:eq(0)").text());
+                $('#uptofansb').html($(tr_id).find("td:eq(4)").text());
+                $("#edittofdiv").show();
+                $('html, body').animate({
+                    scrollTop: $("#edittofdiv").offset().top
+                }, 2000);
+                $.fn.update_tof_btn(tr_id);
+            } else {
+                $('#mcqquestionupdate').val($(tr_id).find("td:eq(0)").text());
+                $('#firstmcqansup').val($(tr_id).find("td:eq(1)").text());
+                $('#secondmcqansup').val($(tr_id).find("td:eq(2)").text());
+                $('#thirdmcqansup').val($(tr_id).find("td:eq(3)").text());
+                $('#upmcqansb').html($(tr_id).find("td:eq(4)").text());
+                $("#editmcqdiv").show();
+                $('html, body').animate({
+                    scrollTop: $("#editmcqdiv").offset().top
+                }, 2000);
+                $.fn.update_mcq_btn(tr_id);
+            }
+
+        });
+
+    };
+
+    $("#addqbtn").click(function () { //add btn fuction
 
 
         var type = $.trim($('#typebtn').html());
@@ -78,7 +223,6 @@ $(document).ready(function () {
         var ans2 = $('#ans2').val();
         var ans3 = $('#ans3').val();
 
-        // console.log(mac);
         if (type != 'Choose type') {
             type_conditon = true;
         }
@@ -132,9 +276,58 @@ $(document).ready(function () {
             tof_valid = true;
         }
 
-
         if (type_conditon && question_valid && (mcq_valid || tof_valid)) {
+
+            var tr = `
+            <tr id='tr_` + index + `'>
+            <td>` + question + `</td>`;
+            if (mcq_valid) {
+                tr += `<td>` + ans1 + `</td>
+                <td>` + ans2 + `</td>
+                <td>` + ans3 + `</td>
+                <td>` + mcq_rightans_value + `</td>`;
+            } else {
+                tr += `<td>True</td>
+                <td>False</td>
+                <td>NULL</td>
+                <td>` + tof_rightans_value + `</td>`;
+            }
+            tr += `<td>`;
+            if (mcq_valid) {
+                tr += `
+            <a class="btn btn-warning btn-icon-split editbtn" id="mcq_edit_` + index + `">`;
+            } else {
+                tr += `
+                <a class="btn btn-warning btn-icon-split editbtn" id="tof_edit_` + index + `">`;
+            }
+            tr += `<span class="icon text-white-50">
+            <i class="fas fa-info-circle"></i>
+            </span>
+            <span class="text " >Edit</span>
+            </a>
+            </td>
+            <td>`;
+            if (mcq_valid) {
+                tr += `
+            <a class="btn btn-danger btn-icon-split removebtn" id="mcq_remove_` + index + `">`;
+            } else {
+                tr += `
+                <a class="btn btn-danger btn-icon-split removebtn" id="tof_remove_` + index + `">`;
+            }
+            tr += `<span class="icon text-white-50">
+            <i class="fas fa-trash"></i>
+            </span>
+            <span class="text">Remove</span>
+            </a>
+            </td>
+            </tr>`;
+            jQuery("#questionsTable").append(tr);
             $("#questionstable").show();
+            $.fn.hardReset();
+            index++;
         }
+        $.fn.editbtnlistener();
+        $.fn.removebtnlistener();
+
     });
 });
